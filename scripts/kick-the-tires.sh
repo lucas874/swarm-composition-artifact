f328f1081565934e79ce36389852550ad96f8ecb
lucas874 <lsc500@outlook.dk>
2025-04-17 18:17:25 +0200

    Better use of pv

--- scripts/kick-the-tires.sh
+++ scripts/kick-the-tires.sh
@@ -12,7 +12,9 @@ error_and_exit() {
 logfile=$LOG_DIR/report.log
 machine_logfile=$LOG_DIR/machines.log
 ax_logfile=$LOG_DIR/ax_all.log
-num_files=8
+num_files=4
+msg_acc="[1/3] Shortened accuracy experiment"
+msg_perf="[2/3] Shortened performance experiment"
 rm -rf $SHORT_CRITERION_DATA_DIR
 mkdir -p $SHORT_CRITERION_DATA_DIR
 rm -rf $SHORT_ACCURACY_RESULT_DIR
@@ -25,13 +27,10 @@ echo "Starting the kick-the-tires script. It may take a minute to start."
 
 cd $MACHINE_CHECK_DIR
 echo "--Shortened accuracy test began at: $(date)--" >> $logfile
-cargo test -- --ignored --nocapture --exact short_run_bench_sub_sizes_general 2>&1 | tee -a $logfile | grep --line-buffered "done-special-symbol" | pv -l -s 4 >> $LOG_DIR/matches.log
-#bash $DIR/scripts/monitor_progress_acc.sh $SHORT_ACCURACY_RESULT_DIR $num_files "[1/3] Shortened accuracy experiment" $logfile "short"
+cargo test -- --ignored --nocapture --exact short_run_bench_sub_sizes_general 2>&1 | tee -a $logfile | grep --line-buffered "done-special-symbol" | pv -N "$msg_acc" -l -t -p -s $num_files >> $LOG_DIR/matches.log
 echo "--Shortened accuracy ended at: $(date)--" >> $logfile
 echo "--Shortened performance test began at: $(date)--" >> $logfile
-#cargo criterion --offline --output-format quiet --plotting-backend disabled --bench composition_benchmark_short >> $logfile 2>&1 &
-cargo criterion --offline --output-format quiet --plotting-backend disabled --bench composition_benchmark_short 2>&1 | tee -a $logfile | grep --line-buffered "done-special-symbol" | pv -l -s 4 >> $LOG_DIR/matches.log
-#bash $DIR/scripts/monitor_progress_perf.sh $SHORT_CRITERION_DATA_DIR $num_files "[2/3] Shortened performance experiment" $logfile "short"
+cargo criterion --offline --output-format quiet --plotting-backend disabled --bench composition_benchmark_short 2>&1 | tee -a $logfile | grep --line-buffered "done-special-symbol" | pv -N "$msg_perf" -l -t -p -s $num_files >> $LOG_DIR/matches.log
 echo "--Shortened performance test ended at: $(date)--" >> $logfile
 echo "--Entering "$PROCESS_RES_DIR" and generating plots at: $(date)--" >> $logfile
 cd $PROCESS_RES_DIR
