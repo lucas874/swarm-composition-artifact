05ce64e10cc95a47c29b83c01f8146ed3607af6f
lucas874 <lsc500@outlook.dk>
2025-04-23 13:48:21 +0200

    Add PowerShell scripts and update README.md. And remove things.txt

---
+++ run_shell.ps1
@@ -0,0 +1,7 @@
+$pwd = Get-Location
+
+docker run -it --rm `
+  -v "$pwd\demos:/ecoop25_artifact/demos" `
+  -v "$pwd\results:/ecoop25_artifact/results" `
+  -v "$pwd\logs:/ecoop25_artifact/logs" `
+  ecoop25_artifact
---
+++ run_shell_no_volume.ps1
@@ -0,0 +1,2 @@
+docker run -it --rm ecoop25_artifact
+  
